331a2432beb1b672191e0f0dd708d614a0a6244a
Eddie <Eddie_yang7@163.com>
2025-04-10 20:58:32 +0800

    添加了订单列表的tabs

--- admin/src/pages/Order/OrderList.jsx
+++ admin/src/pages/Order/OrderList.jsx
@@ -1,5 +1,47 @@
+import { Card, Tabs, Table } from "antd";
+
 const OrderList = () => {
-  return <div className="order-list-container">订单列表内容</div>;
+  const onChange = (key) => {
+    console.log(key);
+  };
+  // 订单状态
+  const status = [
+    {
+      key: "all",
+      label: <span style={{ fontSize: "20px" }}>全部</span>,
+    },
+    {
+      key: "pending",
+      label: <span style={{ fontSize: "20px" }}>未接单</span>,
+    },
+    {
+      key: "processing",
+      label: <span style={{ fontSize: "20px" }}>制作中</span>,
+    },
+    {
+      key: "completed",
+      label: <span style={{ fontSize: "20px" }}>已完成</span>,
+    },
+  ];
+  return (
+    <div className="order-list-container">
+      <Card>
+        {/* 订单状态 */}
+        <Tabs
+          defaultActiveKey="pending"
+          centered
+          items={status}
+          onChange={onChange}
+        />
+
+        {/* 订单列表 */}
+        <Table
+          
+          pagination={false}
+        />
+      </Card>
+    </div>
+  );
 };
 
-export default OrderList; 
+export default OrderList;
